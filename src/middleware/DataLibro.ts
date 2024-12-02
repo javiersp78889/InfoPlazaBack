93cedcc251260802c12a3f525bbda6f15905f47c
javiersp78889 <javiersp78889@gmail.com>
2024-12-02 12:14:30 -0500

    Update DataLibro.ts

--- src/middleware/DataLibro.ts
+++ src/middleware/DataLibro.ts
@@ -1,9 +1,9 @@
 export const DataLibro = async (req, res, next) => {
-    const { nombre, autor, estante, cantidad } = req.body
-    if (!nombre || !autor || !estante || !cantidad) {
+    const { nombre, autor, estante, cantidad, numero } = req.body
+    if (!nombre || !autor || !estante || !cantidad || !numero) {
         res.status(401).json({ mensaje: 'Faltan datos' })
     }
-    else{
+    else {
         next()
     }
 }
