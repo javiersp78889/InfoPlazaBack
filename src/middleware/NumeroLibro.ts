a54b1ea90772fc8a676b6d8379891afa56242c07
javiersp78889 <javiersp78889@gmail.com>
2024-12-02 10:57:22 -0500

    Create NumeroLibro.ts

---
+++ src/middleware/NumeroLibro.ts
@@ -0,0 +1,21 @@
+import Libros from "../models/Libros.models"
+
+export const NumeroLibro = async (req, res, next) => {
+    const { numero } = req.body
+    const numeracion = numero.trim()
+    try {
+
+        const response = await Libros.findAll({ where: { numero: numeracion } })
+        console.log('VERIFICADOR')
+        console.log(response)
+        if (response.length > 0) {
+            console.log('mayor a 0')
+            res.status(401).json({ mensaje: 'Este Libro ya existe' })
+        } else {
+            next()
+        }
+
+    } catch (error) {
+
+    }
+}
