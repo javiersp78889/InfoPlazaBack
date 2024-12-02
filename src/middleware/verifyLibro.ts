a18ae98c1b37693e9a93f63505d7dcb57c1f3282
javiersp78889 <javiersp78889@gmail.com>
2024-12-02 11:16:00 -0500

    Update verifyLibro.ts

--- src/middleware/verifyLibro.ts
+++ src/middleware/verifyLibro.ts
@@ -2,13 +2,14 @@ import Libros from "../models/Libros.models"
 
 export const verifyLibro = async (req, res, next) => {
     const { nombre } = req.body
-    const name = nombre.toLowerCase()
+    const name = nombre.toLowerCase().trim()
     try {
 
         const response = await Libros.findAll({ where: { nombre: name } })
         console.log('VERIFICADOR')
         console.log(response)
         if (response.length > 0) {
+            console.log('mayor a 0')
             res.status(401).json({ mensaje: 'Este Libro ya existe' })
         } else {
             next()
