d4442c6e4ed5743c0ec6d55c65e7182d38abad8a
javiersp78889 <javiersp78889@gmail.com>
2024-12-02 10:57:55 -0500

    Update routerLibros.ts

--- src/routers/routerLibros.ts
+++ src/routers/routerLibros.ts
@@ -6,7 +6,7 @@ const router = Router()
 
 
 router.post('/find', findLibros)
-router.post('/', DataLibro, verifyLibro, createLibros)
+router.post('/', DataLibro, verifyLibro,NumeroLibro, createLibros)
 
 
 
